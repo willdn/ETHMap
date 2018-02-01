a045f227e63c610eed1ba0db673c157736a40663
William Dens <denswilliam@hotmail.com>
2018-02-01 17:05:57 +0100

    Add live network settings

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
 const config = {
+  infuraAPIKey: 'UIWcVZuPtuXHpAFglFyd',
   contractAddressDev: '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf',
-  contractAddressRopsten: '0xa7d1f40478208d2ed065c2ed09cb418d0fdceea0'
+  contractAddressRopsten: '0xa7d1f40478208d2ed065c2ed09cb418d0fdceea0',
+  contractAddressLive: ''
 }
 
 export default config
