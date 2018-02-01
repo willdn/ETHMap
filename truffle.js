6650f6398fcabc99274e5579818f9480b1df1814
William Dens <denswilliam@hotmail.com>
2018-02-01 20:38:21 +0100

    Deploy config

--- truffle.js
+++ truffle.js
@@ -2,22 +2,22 @@ var ganache = require('ganache-cli')
 var HDWalletProvider = require('truffle-hdwallet-provider')
 var dotenv = require('dotenv').config()
 
-var infuraKey = 'ZAG8Wc8qPxdO3cfopRgd'
+var infuraKey = 'UIWcVZuPtuXHpAFglFyd'
 var mnemonic = dotenv.parsed.PASSPHRASE
 
 module.exports = {
   contracts_build_directory: './static/contracts',
-  live: {
-    provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/" + infuraKey, 0),
-    network_id: 1,
-    gasPrice: "10000000000",
-    gas: "5000000"
-  },
   test: {
     provider: ganache.provider(), // in-memory TestRPC provider
     network_id: '*' // Match any network id
   },
   networks: {
+    live: {
+      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/" + infuraKey, 0),
+      network_id: 1,
+      gasPrice: '22000000000',
+      gas: '2000000'
+    },
     development: {
       host: 'localhost',
       port: 8545,
